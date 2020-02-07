e7951cbb79add14e53377a94ce880c19b0b1b726
thomasdasilva65 <thomasdasilva65@penguin>
2020-02-07 11:11:10 +0100

    Début balle et terrain, pas de physique

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
-
-//let container, w, h, scene, camera, controls, renderer, stats;
-//let loop = {};
-//let torus;
-let container, w, h, scene, camera, controls, renderer;
-let materialArray
-let texture_ft, texture_bk, texture_up, texture_dn, texture_lf, texture_rt;
-let skybox, skyboxGeo;
+let container, w, h, scene, camera, controls, renderer, stats;
+let loop = {};
+let balle;
 
 window.addEventListener('load', go);
-//window.addEventListener('resize', resize);
+window.addEventListener('resize', resize);
 
 function go() {
   console.log("Go!");
@@ -16,12 +11,6 @@ function go() {
   gameLoop();
 }
 
-function render() {
-  requestAnimationFrame( render );
-  //controls.update();
-  renderer.render( scene, camera );
-}
-
 function init() {
   container = document.querySelector('#SIApp');
   w = container.clientWidth;
@@ -29,59 +18,23 @@ function init() {
 
   scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(75, w/h, 0.001, 100);
-  camera.position.set(0, 0, -30);
+//Camera
+  camera = new THREE.PerspectiveCamera(75, w/h, 0.1, 1000);
+  camera.position.set(0, -10, 5);
+  //camera.position.x=0;
+  //camera.position.y=-10;
+  //camera.position.z=5;
+  camera.rotation.x=3.14/4;
 
-  const renderConfig = {antialias: true, alpha: true};
-  renderer = new THREE.WebGLRenderer(renderConfig);
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(w, h);
-  container.appendChild(renderer.domElement);
-
-  controls = new THREE.OrbitControls(camera);
-  controls.addEventListener('change', render());
-
-  // Skybox
-
-  materialArray = [];
-  texture_ft = new THREE.TextureLoader().load('/medias/images/arid2_ft.jpg');
-  texture_bk = new THREE.TextureLoader().load('/medias/images/arid2_bk.jpg');
-  texture_up = new THREE.TextureLoader().load('/medias/images/arid2_up.jpg');
-  texture_dn = new THREE.TextureLoader().load('/medias/images/arid2_dn.jpg');
-  texture_lf = new THREE.TextureLoader().load('/medias/images/arid2_lf.jpg');
-  texture_rt = new THREE.TextureLoader().load('/medias/images/arid2_rt.jpg');
-
-  materialArray.push(new THREE.MeshBasicMeterial({map: texture_ft}));
-  materialArray.push(new THREE.MeshBasicMeterial({map: texture_bk}));
-  materialArray.push(new THREE.MeshBasicMeterial({map: texture_up}));
-  materialArray.push(new THREE.MeshBasicMeterial({map: texture_dn}));
-  materialArray.push(new THREE.MeshBasicMeterial({map: texture_lf}));
-  materialArray.push(new THREE.MeshBasicMeterial({map: texture_rt}));
-
-  skyboxGeo = new THREE.BoxGeometry(100000, 100000, 100000);
-  skybox = new THREE.Mesh(skyboxGeo, materialArray);
-  scene.add(skybox);
-}
-
-function gameLoop() {
-  renderer.render(scene,camera);
-  requestAnimationFrame(gameLoop);
-}
-/*
-function init() {
-  container = document.querySelector('#SIApp');
-  w = container.clientWidth;
-  h = container.clientHeight;
-
-  scene = new THREE.Scene();
-
-  camera = new THREE.PerspectiveCamera(75, w/h, 0.001, 100);
-  camera.position.set(0, 0, -30);
+  //camera = new THREE.PerspectiveCamera( 5, window.innerWidth / window.innerHeight, 1, 1000 );
+	//camera.position.set( 0, 75, 100 );
+  //camera.position.z=5;
 
   controls = new THREE.TrackballControls(camera, container);
   controls.target = new THREE.Vector3(0, 0, 0.75);
   controls.panSpeed = 0.4;
 
+//Render
   const renderConfig = {antialias: true, alpha: true};
   renderer = new THREE.WebGLRenderer(renderConfig);
   renderer.setPixelRatio(window.devicePixelRatio);
@@ -95,10 +48,21 @@ function init() {
   document.body.appendChild( stats.domElement );
 
   // add some geometries
-  const geometry = new THREE.TorusGeometry( 10, 3, 16, 100, );
-  const material = new THREE.MeshNormalMaterial( );
-  torus = new THREE.Mesh( geometry, material, );
-  scene.add( torus );
+  //Balle
+  const geometryBalle = new THREE.SphereGeometry( 0.5, 20, 20);
+  const materialBalle = new THREE.MeshNormalMaterial( );
+  //const textureBalle = new THREE.TextureLoader().load('medias/images/neige.jpg');
+  //textureBalle.needsUpdate = true;
+  //const materialBalle = new THREE.MeshFaceMaterial( { map : textureBalle } );
+  balle = new THREE.Mesh( geometryBalle, materialBalle, );
+  scene.add( balle );
+
+  //Terrain
+
+  const geometryTerrain = new THREE.PlaneBufferGeometry( 10, 10 );
+  const materialTerrain = new THREE.MeshBasicMaterial ();
+  terrain = new THREE.Mesh(geometryTerrain, materialTerrain);
+  scene.add( terrain );
 
   const fps  = 60;
   const slow = 1; // slow motion! 1: normal speed, 2: half speed...
@@ -132,7 +96,7 @@ function gameLoop() {
 
 function update(step) {
   const angleIncr = Math.PI * 2 * step / 5 ; // une rotation complète en 5 secondes
-  torus.rotateY(angleIncr);
+  balle.rotateY(angleIncr);
 }
 
 function resize() {
@@ -146,4 +110,3 @@ function resize() {
 function timestamp() {
   return window.performance.now();
 }
-*/
